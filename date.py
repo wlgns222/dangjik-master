3654fd757caaf310186a484257aa6e14d1158634
wlgns222 <rnjswlgns222@daum.net>
2026-01-25 19:41:33 +0900

    Delete date.py

--- date.py
+++
@@ -1,8 +0,0 @@
-import calendar
-
-def get_date_list(year, month):
-    return [day.strftime("%Y-%m-%d") for week in calendar.Calendar().monthdatescalendar(year, month)  for day in week if day.month == month]
-
-
-def is_date_in_range(target_date, start_date, end_date):
-    return start_date <= target_date <= end_date
